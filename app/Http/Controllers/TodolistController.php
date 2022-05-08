4a7c7074a2420af09adef382c73b293149dbcfe4
midoon <harunkoding@gmail.com>
2022-05-08 19:09:43 +0700

    finished

--- app/Http/Controllers/TodolistController.php
+++ app/Http/Controllers/TodolistController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Services\TodolistService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class TodolistController extends Controller
@@ -24,10 +25,23 @@ class TodolistController extends Controller
     }
 
     public function addTodo(Request $request){
+        $todo = $request->input("todo");
+        if(empty($todo)){
+            $todolist = $this->todolistService->getTodolist();
+            return response()->view("todolist.todolist",[
+                "title"=>"TodoList",
+                "todolist" => $todolist,
+                "error" => "Todo is required"
+            ]);
+        }
 
+        $this->todolistService->saveTodo(uniqid(), $todo);
+
+        return redirect()->action([TodolistController::class, "todoList"]);
     }
 
-    public function removeTodo(Request $request, string $todoId){
-
+    public function removeTodo(Request $request, string $todoId): RedirectResponse{
+        $this->todolistService->removeTodo($todoId);
+        return redirect()->action([TodolistController::class, "todoList"]);
     }
 }
--- tests/Feature/TodolistControllerTest.php
+++ tests/Feature/TodolistControllerTest.php
@@ -28,4 +28,41 @@ class TodolistControllerTest extends TestCase
             ->assertSeeText("2")
             ->assertSeeText("Kurniawan");
     }
+
+    public function testAddTodoFailed()
+    {
+        $this->withSession([
+            "user" => "khannedy"
+        ])->post("/todolist", [])
+            ->assertSeeText("Todo is required");
+    }
+
+    public function testAddTodoSuccess()
+    {
+        $this->withSession([
+            "user" => "khannedy"
+        ])->post("/todolist", [
+            "todo" => "Eko"
+        ])->assertRedirect("/todolist");
+    }
+
+    public function testRemoveTodolist()
+    {
+        $this->withSession([
+            "user" => "khannedy",
+            "todolist" => [
+                [
+                    "id" => "1",
+                    "todo" => "Eko"
+                ],
+                [
+                    "id" => "2",
+                    "todo" => "Kurniawan"
+                ]
+            ]
+        ])->post("/todolist/1/delete")
+            ->assertRedirect("/todolist");
+    }
+
+
 }
